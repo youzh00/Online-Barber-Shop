77b36f0ab568ee02e8f31381d6a9f7ced57fe2fb
Elab4d1 <hassanflv@gmail.com>
2023-03-01 14:12:43 +0100

    frontend add modal

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,12 +3,14 @@ import { barberRouter } from "./routers/barber";
 import { userRouter } from "./routers/user";
 import { shopRouter } from "./routers/shop";
 import { haircutRouter } from "./routers/haircut";
+import { serviceRouter } from "./routers/service";
 
 export const appRouter = createTRPCRouter({
   barber: barberRouter,
   shop: shopRouter,
   haircut: haircutRouter,
   user: userRouter,
+  service: serviceRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/service.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+const services = [
+  "Haircut",
+  "Mens haircut",
+  "Womens haircut",
+  "Kids haircut",
+  "Beard trim",
+  "Beard shave",
+  "Beard dye",
+  "Beard trim and shave",
+  "Beard trim and dye",
+  "Beard shave and dye",
+  "Beard trim, shave and dye",
+  "Haircut and beard trim",
+  "Haircut and beard shave",
+  "Haircut and beard dye",
+  "Haircut and beard trim and shave",
+  "Haircut and beard trim and dye",
+  "Haircut and beard shave and dye",
+  "Haircut and beard trim, shave and dye",
+  "Haircut and beard shave and dye",
+];
+
+export const serviceRouter = createTRPCRouter({
+  searchServices: protectedProcedure
+    .input(
+      z.object({
+        query: z.string().min(1).max(100),
+      })
+    )
+    .mutation(({ input }) => {
+      return services.filter((service) =>
+        service.toLowerCase().includes(input.query.toLowerCase())
+      );
+    }),
+});
