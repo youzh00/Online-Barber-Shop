f3f2c057b7ba13524901e84b689e989b446f986b
Elab4d1 <hassanflv@gmail.com>
2023-03-19 15:49:13 +0000

    frontend add save handler

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,22 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log(req.nextauth.token);
+    const token = req.nextauth.token;
+
+    if (req.nextUrl.pathname.startsWith("/home") && token?.role !== "BARBER") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) => token?.role === "BARBER",
+      authorized: ({ token }) => {
+        return Boolean(token?.email);
+      },
     },
   }
 );
 
-export const config = { matcher: ["/home"] };
+export const config = { matcher: ["/home", "/settings"] };
